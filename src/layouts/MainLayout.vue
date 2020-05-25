466a65bc9c983501f1c637fe0aa6998cf1693237
Al Gae <algae1234567890@gmail.com>
2020-05-26 05:00:47 +0800

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-layout view="hHh LpR lFr">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="left = !left" />
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+          </q-avatar>
+          Issue Tracker
+        </q-toolbar-title>
+
+        <q-btn dense flat round icon="menu" @click="right = !right" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="left" side="left" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-drawer show-if-above v-model="right" side="right" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      left: false,
+      right: false
+    }
+  }
+}
+</script>
