2243a142f26759aa850cc1201289a72e266172bf
Al Gae <algae1234567890@gmail.com>
2020-06-05 20:13:16 +0800

    add layout for guest

---
+++ src/layouts/Guest.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-layout view="hHh LpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+          </q-avatar>
+          Tissue - Issue Tracker App
+        </q-toolbar-title>
+        <q-bar class="bg-transparent">
+          <q-space />
+          <q-btn flat v-if="$q.screen.gt.sm" to="/login" exact>
+            Log In
+            <q-tooltip>My Cart</q-tooltip>
+          </q-btn>
+          <!--<q-btn flat v-if="$q.screen.gt.sm" :to="{ name: 'chat' }" exact>
+            Sign Up
+            <q-tooltip>Messages</q-tooltip>
+          </q-btn>-->
+        </q-bar>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      left: false,
+      right: false
+    }
+  }
+}
+</script>
