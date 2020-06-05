233b3b82db2da49b1618b9db62ba1a0396cbde27
Al Gae <algae1234567890@gmail.com>
2020-06-05 20:17:07 +0800

    add auth page

---
+++ src/pages/AuthPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page padding class="fit" style="max-width: 50%;">
+    <div class="row q-gutter-md" style="height: 30vh;">
+      <router-view />
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+}
+</script>
