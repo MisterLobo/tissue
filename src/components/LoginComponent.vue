880a2899ab1c4727eba4fd9e26bf33df71b282c3
Al Gae <algae1234567890@gmail.com>
2020-06-05 20:17:19 +0800

    add login component

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="self-center">
+    <q-btn icon="mdi-github" @click="AuthProvider('github')">Log in with Github</q-btn>
+    <q-btn icon="mdi-google" @click="AuthProvider('google')">Log in with Google</q-btn>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+export default {
+  name: 'LoginComponent',
+  data () {
+    return {}
+  },
+  computed: {
+    ...mapGetters({
+      getUser: 'user/getUserState'
+    }),
+    ...mapActions({
+      newUser: 'user/storeUser'
+    })
+  },
+  methods: {
+
+    AuthProvider (provider) {
+      this.$auth.authenticate(provider).then(response => {
+        this.SocialLogin(provider, response)
+      }).catch(err => {
+        console.log({ err: err })
+      })
+    },
+
+    SocialLogin (provider, response) {
+      this.$http.post(process.env.AUTH_API_URL + provider, response).then(response => {
+        console.log(response.data)
+        const user = response.data
+        this.$store.dispatch('user/storeUser', user)
+        const userstate = this.getUser
+        console.log(userstate)
+        this.$router.push('/')
+      }).catch(err => {
+        console.log({ err: err })
+      })
+    }
+
+  }
+}
+</script>
