e6ad4e2c0ea73027bd327e1e66327e392d6811a7
Al Gae <algae1234567890@gmail.com>
2020-06-05 21:23:16 +0800

    add shell scripts

---
+++ scripts/denv.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+PASSPHRASE=$DEV_GPG_SECRET
+FILE=devenv.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" --output ./env "$FILE"
+cp env ./public
---
+++ scripts/env.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+PASSPHRASE=$PROD_GPG_SECRET
+FILE=env.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" --output ./env "$FILE"
+cp env ./public
