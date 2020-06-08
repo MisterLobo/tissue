3715b8dd626308ac21f705116fd42a9de28da737
Al Gae <algae1234567890@gmail.com>
2020-06-08 20:19:25 +0800

    update check-auth boot script

--- src/boot/check-auth.js
+++ src/boot/check-auth.js
@@ -1,14 +1,27 @@
-// import something here
+import { SessionStorage } from 'quasar'
 
-// "async" is optional;
-// more info on params: https://quasar.dev/quasar-cli/cli-documentation/boot-files#Anatomy-of-a-boot-file
 // eslint-disable-next-line @typescript-eslint/require-await
-export default async ({ store, redirect, urlPath }) => {
+export default async ({ store, redirect, urlPath, router }) => {
   console.log(urlPath)
-  if (urlPath === '/login') return
-  if (urlPath !== '/' && urlPath.contains('/auth')) return
+  // if (urlPath === '/login') return
+  console.log('first')
+  if (urlPath.toString().includes('/auth')) return
   if (store.getters['user/isAuthenticated'] !== true) {
-    console.log('Not logged in. Redirecting')
-    await redirect('/login')
+    const userToken = SessionStorage.getItem('user_token')
+    if (userToken !== undefined && userToken !== null && userToken !== '') {
+      store.dispatch('user/check', true)
+      console.log(userToken)
+      const provider = SessionStorage.getItem('auth_provider')
+      store.dispatch('user/fetchUser', { provider, userToken }).then(() => {
+        store.dispatch('user/check', false)
+        console.log(urlPath)
+        router.push('/')
+      })
+    }
+    else {
+      if (urlPath === '/login') return
+      console.log('Not logged in. Redirecting')
+      await redirect('/login')
+    }
   }
 }
