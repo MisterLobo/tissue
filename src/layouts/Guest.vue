36942f8a6fb6b8d30a9edd675a71cd607ec55418
Al Gae <algae1234567890@gmail.com>
2020-06-08 20:19:58 +0800

    added home button for Guest

--- src/layouts/Guest.vue
+++ src/layouts/Guest.vue
@@ -4,7 +4,7 @@
     <q-header elevated class="bg-primary text-white">
       <q-toolbar>
 
-        <q-toolbar-title>
+        <q-toolbar-title @click="goHome" class="cursor-pointer">
           <q-avatar>
             <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
           </q-avatar>
@@ -12,9 +12,11 @@
         </q-toolbar-title>
         <q-bar class="bg-transparent">
           <q-space />
+          <q-btn flat v-if="$q.screen.gt.sm" to="/" exact>
+            Home
+          </q-btn>
           <q-btn flat v-if="$q.screen.gt.sm" to="/login" exact>
             Log In
-            <q-tooltip>My Cart</q-tooltip>
           </q-btn>
           <!--<q-btn flat v-if="$q.screen.gt.sm" :to="{ name: 'chat' }" exact>
             Sign Up
@@ -37,6 +39,11 @@ export default {
     return {
       left: false,
       right: false
+    }
+  },
+  methods: {
+    goHome () {
+      this.$router.push('/')
     }
   }
 }
