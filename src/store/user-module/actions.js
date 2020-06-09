c9dc3ab4d3743dca28dcee796c9ad66d52f3e777
Al Gae <algae1234567890@gmail.com>
2020-06-09 18:19:11 +0800

    append auth header

--- src/store/user-module/actions.js
+++ src/store/user-module/actions.js
@@ -7,7 +7,7 @@ export function storeUser ({ commit }, user) {
 export function fetchUser ({ commit }, { provider, token }) {
   console.log('token: ', token)
   const userToken = SessionStorage.getItem('user_token')
-  return axios.post(`${process.env.API_URL}/social/getuser/${provider}`, { token: userToken })
+  return axios.post(`${process.env.API_URL}/social/getuser/${provider}`, { token: userToken }, { auth: { username: process.env.AUTH_USER, password: process.env.AUTH_PASSWORD } })
     .then(response => {
       const user = response.data
       console.log('user: ', user)
