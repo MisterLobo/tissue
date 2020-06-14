312c3bc8e3afffe15d5172fea709fca297180e28
Al Gae <algae1234567890@gmail.com>
2020-06-14 22:40:57 +0800

    added prefetch in Index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -16,6 +16,23 @@ import ExampleComponent from 'components/CompositionComponent.vue'
 import { Todo, Meta } from 'components/models'
 
 export default Vue.extend({
+  preFetch ({ store, /* currentRoute, previousRoute, */ redirect }) {
+    // fetch data, validate route and optionally redirect to some other route...
+
+    // ssrContext is available only server-side in SSR mode
+
+    // No access to "this" here as preFetch() is called before
+    // the component gets instantiated.
+
+    // Return a Promise if you are running an async job
+    // Example:
+    if (store.getters['user/isAuthenticated'] === false && store.getters['user/isChecked'] === false) {
+      // console.log('second')
+      console.log('Not logged in. Redirecting ..')
+      return redirect('/login')
+    }
+    // return store.dispatch('fetchItem', currentRoute.params.id)
+  },
   name: 'PageIndex',
   components: { ExampleComponent },
   data () {
