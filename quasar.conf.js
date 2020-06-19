6fcf9336548947100e6a5c8b255632a79186a5ee
Al Gae <algae1234567890@gmail.com>
2020-06-20 04:09:38 +0800

    change dev server host

--- quasar.conf.js
+++ quasar.conf.js
@@ -175,6 +175,7 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
+      host: 'app.tissueapplocal.xyz',
       port: 8080,
       open: true // opens browser window automatically
     },
