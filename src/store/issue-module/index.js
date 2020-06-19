b881e82b08ca054c71946b6e4e9a475b043b1b50
Al Gae <algae1234567890@gmail.com>
2020-06-20 04:17:21 +0800

    updated issue state module

--- src/store/issue-module/index.js
+++ src/store/issue-module/index.js
@@ -3,14 +3,6 @@ import * as getters from './getters'
 import * as mutations from './mutations'
 import * as actions from './actions'
 
-export class CommentClass {
-  id
-  authorId
-  body
-  upVotes
-  downVotes
-}
-
 export default {
   namespaced: true,
   state,
